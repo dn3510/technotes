92a54c6d1bc6fff01533d4302d8451095f5dcbba
Bing de la Cruz <104107405+dn3510@users.noreply.github.com>
2022-12-24 16:12:16 +0800

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setCredentials } from '../../features/auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://technotes-blh5.onrender.com',
+  baseUrl: 'https://technotev1.onrender.com',
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
